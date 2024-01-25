649fb06baf2cb098a6803b20334ae59c30ad68b5
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-01-25 07:10:54 -0300

    HTML inicial.

---
+++ palabras.js
@@ -0,0 +1,1 @@
+const words = ['Carne', 'Martillo', 'Lavadora', 'Sucio', 'Cangrejo', 'Caracol', 'Abajo', 'Alumno', 'Bonito', 'Cesta', 'Sol', 'Beber', 'Botella', 'Hamburguesa', 'Invierno', 'Delgado'];
