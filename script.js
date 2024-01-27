87a0a087690c7dac50cc57b0785a7275c106fd36
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-01-27 06:50:50 -0300

    Establecer constantes para el dibujo y variables para llevar el estado del juego.

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 ctx.canvas.width = 0;
 ctx.canvas.height = 0;
+
+const bodyParts = [
+    [4,2,1,1], 
+    [4,3,1,2],
+    [3,5,1,1],
+    [5,5,1,1],
+    [3,3,1,1],
+    [5,3,1,1]
+];
+
+let selectedWord;
+let usedLetters;
+let mistakes;
+let hits;
+
